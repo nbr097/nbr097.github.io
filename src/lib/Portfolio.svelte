d02ee8ef8aee2e9ec508d363e10be38c9708819f
nbr097 <nicho.brown2@gmail.com>
2025-11-23 22:41:18 +1000

    Revert

---
+++ src/lib/Portfolio.svelte
@@ -0,0 +1,438 @@
+<section class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
+	<div class="text-center pb-12" style=" padding-top:7em; margin-top:-7em;" id="portfolio">
+		<h1
+			class="font-bebas text-4xl md:text-5xl lg:text-6xl font-heading text-black overflow-y-hidden pb-10"
+		>
+			portfolio
+		</h1>
+	</div>
+	<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+				<!-- card -->
+                <div class="antialiased text-gray-900" style="display:grid; place-items:center">
+                    <a href="https://youtu.be/U-6c0MFTe7U" target="_blank">
+                        <div class="">
+                            <img
+                                src="https://img.youtube.com/vi/U-6c0MFTe7U/maxresdefault.jpg"
+                                alt=" random imgee"
+                                class=" h-72 object-cover object-center rounded-lg shadow-lg"
+                            />
+                            <div class="relative px-4 -mt-16 pb-16">
+                                <div class="bg-white p-6 rounded-lg shadow-lg">
+                                    <div class="flex items-baseline">
+                                        <div class=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
+                                            Edited
+                                        </div>
+                                    </div>
+                                    <h4 class="mt-1 text-md font-semibold Capitalize leading-tight truncate">
+                                        Covert Media R34 Challenge
+                                    </h4>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <!-- card -->
+        <!-- card -->
+		<div class="antialiased text-gray-900" style="display:grid; place-items:center">
+			<a href="https://youtu.be/WfWM2dMyMBo" target="_blank">
+				<div class="">
+					<img
+						src="https://img.youtube.com/vi/WfWM2dMyMBo/maxresdefault.jpg"
+						alt=" random imgee"
+						class=" h-72 object-cover object-center rounded-lg shadow-lg"
+					/>
+					<div class="relative px-4 -mt-16 pb-16">
+						<div class="bg-white p-6 rounded-lg shadow-lg">
+							<div class="flex items-baseline">
+								<div class=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
+									Filmed &bull; Edited
+								</div>
+							</div>
+							<h4 class="mt-1 text-md font-semibold Capitalize leading-tight truncate">
+								MS 2020 Recap
+							</h4>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+		<!-- card -->
+		<!-- card -->
+		<div class="antialiased text-gray-900" style="display:grid; place-items:center">
+			<a href="https://youtu.be/ZBewWjfs2jI" target="_blank">
+				<div class="">
+					<img
+						src="https://img.youtube.com/vi/ZBewWjfs2jI/maxresdefault.jpg"
+						alt=" random imgee"
+						class=" h-72 object-cover object-center rounded-lg shadow-lg"
+					/>
+					<div class="relative px-4 -mt-16 pb-16">
+						<div class="bg-white p-6 rounded-lg shadow-lg">
+							<div class="flex items-baseline">
+								<div class=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
+									Filmed &bull; Edited
+								</div>
+							</div>
+							<h4 class="mt-1 text-md font-semibold Capitalize leading-tight truncate">
+								MS Uluru Trip
+							</h4>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+		<!-- card -->
+		<!-- card -->
+		<div class="antialiased text-gray-900" style="display:grid; place-items:center">
+			<a href="https://youtu.be/hXZFJuvSeaw" target="_blank">
+				<div class="">
+					<img
+						src="https://img.youtube.com/vi/hXZFJuvSeaw/maxresdefault.jpg"
+						alt=" random imgee"
+						class=" h-72 object-cover object-center rounded-lg shadow-lg"
+					/>
+					<div class="relative px-4 -mt-16 pb-16">
+						<div class="bg-white p-6 rounded-lg shadow-lg">
+							<div class="flex items-baseline">
+								<div class=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
+									Filmed &bull; Edited
+								</div>
+							</div>
+							<h4 class="mt-1 text-md font-semibold Capitalize leading-tight truncate">
+								MS Toowoomba Formal Shoot
+							</h4>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+		<!-- card -->
+		<!-- card -->
+		<div class="antialiased text-gray-900" style="display:grid; place-items:center">
+			<a href="https://youtu.be/Rj3hp22yssg" target="_blank">
+				<div class="">
+					<img
+						src="https://img.youtube.com/vi/Rj3hp22yssg/maxresdefault.jpg"
+						alt=" random imgee"
+						class=" h-72 object-cover object-center rounded-lg shadow-lg"
+					/>
+					<div class="relative px-4 -mt-16 pb-16">
+						<div class="bg-white p-6 rounded-lg shadow-lg">
+							<div class="flex items-baseline">
+								<div class=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
+									Filmed &bull; Edited
+								</div>
+							</div>
+							<h4 class="mt-1 text-md font-semibold Capitalize leading-tight truncate">
+								MS Paityn City Formal Shoot
+							</h4>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+		<!-- card -->
+		<!-- card -->
+		<div class="antialiased text-gray-900" style="display:grid; place-items:center">
+			<a href="https://youtu.be/IgRutw_DHwU" target="_blank">
+				<div class="">
+					<img
+						src="https://img.youtube.com/vi/IgRutw_DHwU/maxresdefault.jpg"
+						alt=" random imgee"
+						class=" h-72 object-cover object-center rounded-lg shadow-lg"
+					/>
+					<div class="relative px-4 -mt-16 pb-16">
+						<div class="bg-white p-6 rounded-lg shadow-lg">
+							<div class="flex items-baseline">
+								<div class=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
+									Filmed &bull; Edited
+								</div>
+							</div>
+							<h4 class="mt-1 text-md font-semibold Capitalize leading-tight truncate">
+								MS NVSH Formal
+							</h4>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+		<!-- card -->
+		<!-- card -->
+		<div class="antialiased text-gray-900" style="display:grid; place-items:center">
+			<a href="https://youtu.be/7mW3_ymSU_I" target="_blank">
+				<div class="">
+					<img
+						src="https://img.youtube.com/vi/7mW3_ymSU_I/maxresdefault.jpg"
+						alt=" random imgee"
+						class=" h-72 object-cover object-center rounded-lg shadow-lg"
+					/>
+					<div class="relative px-4 -mt-16 pb-16">
+						<div class="bg-white p-6 rounded-lg shadow-lg">
+							<div class="flex items-baseline">
+								<div class=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
+									Filmed &bull; Edited
+								</div>
+							</div>
+							<h4 class="mt-1 text-md font-semibold Capitalize leading-tight truncate">
+								MS JNR Bowling Shoot
+							</h4>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+		<!-- card -->
+		<!-- card -->
+		<div class="antialiased text-gray-900" style="display:grid; place-items:center">
+			<a href="https://youtu.be/nM9D99ElbcI" target="_blank">
+				<div class="">
+					<img
+						src="https://img.youtube.com/vi/nM9D99ElbcI/maxresdefault.jpg"
+						alt=" random imgee"
+						class=" h-72 object-cover object-center rounded-lg shadow-lg"
+					/>
+					<div class="relative px-4 -mt-16 pb-16">
+						<div class="bg-white p-6 rounded-lg shadow-lg">
+							<div class="flex items-baseline">
+								<div class=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
+									Filmed &bull; Edited
+								</div>
+							</div>
+							<h4 class="mt-1 text-md font-semibold Capitalize leading-tight truncate">
+								MS Neon Shoot
+							</h4>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+		<!-- card -->
+		<!-- card -->
+		<div class="antialiased text-gray-900" style="display:grid; place-items:center">
+			<a href="https://youtu.be/ax2sES-pAc0" target="_blank">
+				<div class="">
+					<img
+						src="https://img.youtube.com/vi/ax2sES-pAc0/maxresdefault.jpg"
+						alt=" random imgee"
+						class=" h-72 object-cover object-center rounded-lg shadow-lg"
+					/>
+					<div class="relative px-4 -mt-16 pb-16">
+						<div class="bg-white p-6 rounded-lg shadow-lg">
+							<div class="flex items-baseline">
+								<div class=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
+									Filmed &bull; Edited
+								</div>
+							</div>
+							<h4 class="mt-1 text-md font-semibold Capitalize leading-tight truncate">
+								MS Goldcoast Group Shoot
+							</h4>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+		<!-- card -->
+		<!-- card -->
+		<div class="antialiased text-gray-900" style="display:grid; place-items:center">
+			<a href="https://youtu.be/sDNQ5jmiKEk" target="_blank">
+				<div class="">
+					<img
+						src="https://img.youtube.com/vi/sDNQ5jmiKEk/maxresdefault.jpg"
+						alt=" random imgee"
+						class=" h-72 object-cover object-center rounded-lg shadow-lg"
+					/>
+					<div class="relative px-4 -mt-16 pb-16">
+						<div class="bg-white p-6 rounded-lg shadow-lg">
+							<div class="flex items-baseline">
+								<div class=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
+									Filmed &bull; Edited
+								</div>
+							</div>
+							<h4 class="mt-1 text-md font-semibold Capitalize leading-tight truncate">
+								MS Amber Formal Shoot
+							</h4>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+		<!-- card -->
+        	<!-- card -->
+		<div class="antialiased text-gray-900" style="display:grid; place-items:center">
+			<a href="https://youtu.be/oUtl2T4xiiE" target="_blank">
+				<div class="">
+					<img
+						src="https://img.youtube.com/vi/oUtl2T4xiiE/maxresdefault.jpg"
+						alt=" random imgee"
+						class=" h-72 object-cover object-center rounded-lg shadow-lg"
+					/>
+					<div class="relative px-4 -mt-16 pb-16">
+						<div class="bg-white p-6 rounded-lg shadow-lg">
+							<div class="flex items-baseline">
+								<div class=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
+									Filmed &bull; Edited
+								</div>
+							</div>
+							<h4 class="mt-1 text-md font-semibold Capitalize leading-tight truncate">
+								MS 2020 Cover Shoot
+							</h4>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+		<!-- card -->
+               	<!-- card -->
+		<div class="antialiased text-gray-900" style="display:grid; place-items:center">
+			<a href="https://youtu.be/_quwOv6zKeI" target="_blank">
+				<div class="">
+					<img
+						src="https://img.youtube.com/vi/_quwOv6zKeI/maxresdefault.jpg"
+						alt=" random imgee"
+						class=" h-72 object-cover object-center rounded-lg shadow-lg"
+					/>
+					<div class="relative px-4 -mt-16 pb-16">
+						<div class="bg-white p-6 rounded-lg shadow-lg">
+							<div class="flex items-baseline">
+								<div class=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
+									Filmed &bull; Edited
+								</div>
+							</div>
+							<h4 class="mt-1 text-md font-semibold Capitalize leading-tight truncate">
+								MS 2019 Recap
+							</h4>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+		<!-- card -->
+        <!-- card -->
+		<div class="antialiased text-gray-900" style="display:grid; place-items:center">
+			<a href="https://youtu.be/zolZcnn-de8" target="_blank">
+				<div class="">
+					<img
+						src="https://img.youtube.com/vi/zolZcnn-de8/maxresdefault.jpg"
+						alt=" random imgee"
+						class=" h-72 object-cover object-center rounded-lg shadow-lg"
+					/>
+					<div class="relative px-4 -mt-16 pb-16">
+						<div class="bg-white p-6 rounded-lg shadow-lg">
+							<div class="flex items-baseline">
+								<div class=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
+									Filmed &bull; Edited
+								</div>
+							</div>
+							<h4 class="mt-1 text-md font-semibold Capitalize leading-tight truncate">
+								MS 2019 Cover Shoot
+							</h4>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+		<!-- card -->
+             <!-- card -->
+		<div class="antialiased text-gray-900" style="display:grid; place-items:center">
+			<a href="https://youtu.be/I3APOV2Z3hA" target="_blank">
+				<div class="">
+					<img
+						src="https://img.youtube.com/vi/I3APOV2Z3hA/maxresdefault.jpg"
+						alt=" random imgee"
+						class=" h-72 object-cover object-center rounded-lg shadow-lg"
+					/>
+					<div class="relative px-4 -mt-16 pb-16">
+						<div class="bg-white p-6 rounded-lg shadow-lg">
+							<div class="flex items-baseline">
+								<div class=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
+									Filmed &bull; Edited
+								</div>
+							</div>
+							<h4 class="mt-1 text-md font-semibold Capitalize leading-tight truncate">
+								MS Ashton &amp; Jacob Shoot
+							</h4>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+		<!-- card -->
+        <!-- card -->
+		<div class="antialiased text-gray-900" style="display:grid; place-items:center">
+			<a href="https://youtu.be/VFOMHUCsdrk" target="_blank">
+				<div class="">
+					<img
+						src="https://img.youtube.com/vi/VFOMHUCsdrk/maxresdefault.jpg"
+						alt=" random imgee"
+						class=" h-72 object-cover object-center rounded-lg shadow-lg"
+					/>
+					<div class="relative px-4 -mt-16 pb-16">
+						<div class="bg-white p-6 rounded-lg shadow-lg">
+							<div class="flex items-baseline">
+								<div class=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
+									Filmed &bull; Edited
+								</div>
+							</div>
+							<h4 class="mt-1 text-md font-semibold Capitalize leading-tight truncate">
+								MS 2019 Gallery Gala Night
+							</h4>
+						</div>
+					</div>
+				</div>
+			</a>
+		</div>
+		<!-- card -->
+                <!-- card -->
+                <div class="antialiased text-gray-900" style="display:grid; place-items:center">
+                    <a href="https://youtu.be/qruJADR9lHw" target="_blank">
+                        <div class="">
+                            <img
+                                src="https://img.youtube.com/vi/qruJADR9lHw/maxresdefault.jpg"
+                                alt=" random imgee"
+                                class=" h-72 object-cover object-center rounded-lg shadow-lg"
+                            />
+                            <div class="relative px-4 -mt-16 pb-16">
+                                <div class="bg-white p-6 rounded-lg shadow-lg">
+                                    <div class="flex items-baseline">
+                                        <div class=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
+                                            Filmed &bull; Edited
+                                        </div>
+                                    </div>
+                                    <h4 class="mt-1 text-md font-semibold Capitalize leading-tight truncate">
+                                        MS Amalie Location Shoot
+                                    </h4>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <!-- card -->
+                        <!-- card -->
+        <div class="antialiased text-gray-900" style="display:grid; place-items:center">
+            <a href="https://youtu.be/60tI_iWF8Pk" target="_blank">
+                <div class="">
+                    <img
+                        src="https://img.youtube.com/vi/60tI_iWF8Pk/maxresdefault.jpg"
+                        alt=" random imgee"
+                        class=" h-72 object-cover object-center rounded-lg shadow-lg"
+                    />
+                    <div class="relative px-4 -mt-16 pb-16">
+                        <div class="bg-white p-6 rounded-lg shadow-lg">
+                            <div class="flex items-baseline">
+                                <div class=" text-gray-600 uppercase text-xs font-semibold tracking-wider">
+                                    Filmed &bull; Edited
+                                </div>
+                            </div>
+                            <h4 class="mt-1 text-md font-semibold Capitalize leading-tight truncate">
+                                MS Sarah Location Shoot
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <!-- card -->
+
+        
+	</div>
+</section>
---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Nav from '$lib/Nav.svelte';
+	import Portfolio from '$lib/portfolio.svelte';
+	import Foot from '$lib/Foot.svelte';
+	import About from '$lib/About.svelte';
+	import Hero from '$lib/Hero.svelte';
+</script>
+
+<Nav />
+
+<Hero />
+
+<About />
+
+<Portfolio />
+
+<Foot />
