023ad15469984e93a13cc02484b1509872a36d30
nbr097 <nicho.brown2@gmail.com>
2025-11-23 22:27:18 +1000

    Change Theme

--- build.sh
+++
@@ -1,3 +0,0 @@
-npm run build
-git commit . -m "i luv woody"
-git push origin `git subtree split --prefix build master`:gh-pages --force
--- src/lib/Foot.svelte
+++
@@ -1,3 +0,0 @@
-<footer class="h-1/6 w-screen bg-white text-black text-center text-xs pb-4">
-	<p>&copy; Nicholas Brown Media 2021</p>
-</footer>
