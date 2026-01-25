3a12dcbd072c55ecda01fcf2e03d8c86ff04c2e4
nbr097 <nicho.brown2@gmail.com>
2026-01-25 18:15:04 +1000

    Create promo-banner.php

---
+++ static/promo-banner.php
@@ -0,0 +1,26 @@
+<?php
+// Set timezone to Australian Eastern Standard Time
+date_default_timezone_set('Australia/Brisbane');
+
+// Set the promotion end date (adjust as needed)
+// This will now use AEST/AEDT automatically based on daylight saving
+$endDate = strtotime('2026-01-025 23:59:59'); // End of Jan 25, 2026
+$today = time();
+
+// Set proper image headers
+header('Content-Type: image/jpg');
+header('Cache-Control: no-cache, no-store, must-revalidate');
+header('Pragma: no-cache');
+header('Expires: 0');
+
+if ($today <= $endDate) {
+    // Show promotion banner
+    $imageUrl = 'https://cdn11.bigcommerce.com/s-a579g2rqqa/images/stencil/original/image-manager/desktop-homepage-banner-backtowork1-230126.jpg';
+    $imageData = file_get_contents($imageUrl);
+    echo $imageData;
+} else {
+    // After end date, show a blank 1x1 transparent GIF
+    // This makes the banner "disappear"
+    echo base64_decode('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+}
+?>
